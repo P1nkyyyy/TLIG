7e31e84fa6e7dcfab81e8ade37d8ebcd6c6d3cee
Michal Pinkava <michal.pinkava01@upol.cz>
2026-03-24 20:28:43 +0100

    Rozšíření DB

--- app/src/main/java/com/example/seminar_1/AppDatabase.kt
+++ app/src/main/java/com/example/seminar_1/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.seminar_1.data_classes.Message
 
 @Database(entities = [Message::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
--- app/src/main/java/com/example/seminar_1/MessageDao.kt
+++ app/src/main/java/com/example/seminar_1/MessageDao.kt
@@ -4,12 +4,16 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import com.example.seminar_1.data_classes.Message
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MessagesDao {
     @Query("SELECT * FROM messages")
     fun getAll(): Flow<List<Message>>
+
+    @Query("SELECT * FROM messages WHERE id = :id")
+    fun get(id: Int): Flow<Message>
 
     @Insert
     suspend fun insert(message: Message): Long
